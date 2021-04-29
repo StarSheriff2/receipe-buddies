17d3d6806e7709b10582ec4665d0d827d14bbd5f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-04-29 17:59:13 -0600

    add log_in to Users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       redirect_to @user, notice: "User was successfully created."
     else
       render :new, status: :unprocessable_entity
