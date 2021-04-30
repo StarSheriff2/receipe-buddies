d57134f3ef2121608ac991a6c7a041550813f449
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-04-30 16:02:01 -0600

    create User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   # Model Tests
   context 'with correct params' do
     before do
-      @user = User.create(name: 'Peter Parker', email: 'peter.parker@email.com', password: 'abc123', gravatar_url: '')
+      @user = User.create(username: 'Peter_Parker', fullname: 'Peter Parker')
     end
 
     it 'creates a user' do
@@ -12,13 +12,13 @@ RSpec.describe User, type: :model do
     end
 
     it 'reads a user' do
-      expect(User.find_by(name: 'Peter Parker')).to eq(@user)
+      expect(User.find_by(username: 'Peter_Parker')).to eq(@user)
     end
   end
 
   context 'with incorrect params' do
     before do
-      @user = User.create(email: 'peter.parker@email.com', password: 'abc123', gravatar_url: '')
+      @user = User.create(username: 'Peter Parker', fullname: 'Peter Parker')
     end
 
     it 'cannot create a user' do
@@ -26,68 +26,49 @@ RSpec.describe User, type: :model do
     end
 
     it 'cannot read a user' do
-      expect(User.find_by(name: 'Peter Parker')).not_to eq(@user)
+      expect(User.find_by(username: 'Peter Parker')).not_to eq(@user)
     end
   end
 
   # Association Tests
-  it { should have_many(:posts) }
-  it { should have_many(:comments) }
-  it { should have_many(:likes) }
+  it { should have_many(:opinions) }
 
-  context 'user as an inviter' do
+  context 'user as a follower' do
     before do
-      User.create(name: 'Peter Parker', email: 'peter.parker@email.com', password: 'abc123', gravatar_url: '')
-      User.create(name: 'Jane Watson', email: 'jane_watson@email.com', password: 'abc123', gravatar_url: '')
+      User.create(username: 'Peter_Parker', fullname: 'Peter Parker')
+      User.create(username: 'Jane_Watson', fullname: 'Jane Watson')
     end
 
-    let(:inviter) { User.find_by_name('Peter Parker') }
-    let(:invitee) { User.find_by_name('Jane Watson') }
-    let(:friend_request) { inviter.friendships.build(invitee_id: invitee.id) }
+    let(:follower) { User.find_by_username('Peter_Parker') }
+    let(:followed) { User.find_by_username('Jane_Watson') }
+    let(:new_following) { follower.followings.build(followed_id: followed.id) }
 
-    it { should have_many(:friendships) }
-    it { should have_many(:invitees) }
+    it { should have_many(:followings) }
 
-    it 'can invite another user to be a friend' do
-      expect(friend_request).to be_valid
-    end
-
-    it 'can check the status of a friend request' do
-      friend_request
-      friendship_status = inviter.friendships.first.status
-      expect(friendship_status).to eq(false)
-    end
-
-    it 'can see the name of the invitee' do
-      invitee_user = User.find(friend_request.invitee_id)
-      invitee_user_name = User.find(invitee_user.id).name
-      expect(invitee_user_name).to eq('Jane Watson')
+    it 'can follow another user' do
+      followed_user = User.find(new_following.followed_id)
+      expect(followed_user.username).to eq('Jane_Watson')
+      expect(followed_user.username).to_not eq('Peter_Parker')
     end
   end
 
-  context 'user as an invitee' do
+  context 'user as a followed user' do
     before do
-      User.create(name: 'Peter Parker', email: 'peter.parker@email.com', password: 'abc123', gravatar_url: '')
-      User.create(name: 'Jane Watson', email: 'jane_watson@email.com', password: 'abc123', gravatar_url: '')
+      User.create(username: 'Peter_Parker', fullname: 'Peter Parker')
+      User.create(username: 'Jane_Watson', fullname: 'Jane Watson')
     end
 
-    let(:inviter) { User.find_by_name('Peter Parker') }
-    let(:invitee) { User.find_by_name('Jane Watson') }
-    let(:friend_request) { inviter.friendships.create(invitee_id: invitee.id, status: true) }
+    let(:follower) { User.find_by_username('Peter_Parker') }
+    let(:followed) { User.find_by_username('Jane_Watson') }
+    let(:new_following) { follower.followings.build(followed_id: followed.id) }
 
-    it { should have_many(:inverse_friendships) }
-    it { should have_many(:inviters) }
+    it { should have_many(:inverse_followings) }
+    it { should have_many(:followers) }
 
-    it 'can check the status of a friend request' do
-      friend_request
-      friendship_status = invitee.inverse_friendships.first.status
-      expect(friendship_status).to eq(true)
-    end
-
-    it 'can see the name of the inviter' do
-      inviter_user = User.find(friend_request.inviter_id)
-      inviter_user_name = User.find(inviter_user.id).name
-      expect(inviter_user_name).to eq('Peter Parker')
+    it 'can see the name of a follower' do
+      follower_user = User.find(new_following.follower_id)
+      expect(follower_user.username).to eq('Peter_Parker')
+      expect(follower_user.username).to_not eq('Jane_Watson')
     end
   end
 
@@ -98,8 +79,11 @@ RSpec.describe User, type: :model do
 
   context 'it is valid with valid attributes' do
     describe '#name' do
-      it { should validate_presence_of(:name) }
-      it { should validate_length_of(:name) }
+      it { should validate_presence_of(:username) }
+      it { should_not allow_value('Peter Parker').for(:username) }
+      it { should validate_length_of(:username) }
+      it { should validate_presence_of(:fullname) }
+      it { should validate_length_of(:fullname) }
     end
   end
 end
