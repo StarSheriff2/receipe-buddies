322aa40d946542c0cf58d5de8795d75f3f6df3df
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-04-30 15:05:02 -0600

    add validation constraint to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { maximum: 20 }
+  VALID_USERNAME_REGEX = /\A[\w+\-\_]+\z/i
+  validates :username, presence: true, format: { with: VALID_USERNAME_REGEX }, length: { maximum: 20 }
   validates :fullname, presence: true, length: { maximum: 40 }
 
   has_many :opinions, foreign_key: :author_id, dependent: :destroy
