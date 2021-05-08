dad43d87d24b9dc2b6c8e0f538b74d17a8051ba2
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-05-07 20:28:19 -0600

    edit logged user user-page profile-detailed-info picture and buttons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   def profile_picture_circle(user, dimensions, alt: nil, class_name: nil)
     if user.avatar.attached?
       cl_image_tag(user.avatar.key, width: dimensions, height: dimensions, alt: alt, class: class_name,
-                                    crop: :fill, gravity: :face, radius: "max", fetch_format: :auto)
+                                    crop: :fill, gravity: :face, radius: 'max', fetch_format: :auto)
     else
       image_tag('blank-profile-picture', class: class_name, width: dimensions, height: dimensions, alt: alt)
     end
@@ -20,10 +20,23 @@ module UsersHelper
   end
 
   def cover_picture(user)
-    if user.cover_image.attached?
-      content_tag(:div, "", class: "cover-picture-div", style: "background: url(#{ @user.cover_image.url }) center center/cover;")
+    return unless user.cover_image.attached?
+
+    content_tag(:div, '', class: 'cover-picture-div',
+                          style: "background: url(#{@user.cover_image.url}) center center/cover;")
+  end
+
+  def follow_button(user)
+    if !current_user.following?(user) && current_user != user
+      link_to followings_path(following: { follower_id: current_user, followed_id: user }), method: 'POST' do
+        content_tag(:i, '', class: %w[fas fa-plus-circle])
+      end
+    elsif current_user == user
+      nil
     else
-      return
+      link_to following_path(current_user.following_record(user)), method: 'DELETE' do
+        content_tag(:i, '', class: %w[fas fa-minus-circle])
+      end
     end
   end
 end
